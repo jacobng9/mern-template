966090fcfd917418a2d1cfa75e924e90b11bbd0f
Jacob Ng <110934976+jacobpng9@users.noreply.github.com>
2025-01-01 13:01:46 -0700

    serve static files from server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,15 @@ app.use(cors());
 app.use(express.json());
 app.use("/record", records);
 
+// Serve static files from the React app
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+
 // start the Express server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
