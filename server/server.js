0d371340247370b4d1f0e81a79f37b8945d562fd
Jacob Ng <110934976+jacobpng9@users.noreply.github.com>
2025-01-01 16:24:04 -0700

    fix 3.0 server

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ app.use("/record", records);
 // Serve static files from the React app
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production")
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "/server/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/server/client/dist/index.html"));
 });
 
 
