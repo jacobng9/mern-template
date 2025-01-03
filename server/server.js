ed608e8a1efde27375a5b85a86a88d0d8d9d7de9
Jacob Ng <110934976+jacobpng9@users.noreply.github.com>
2025-01-02 18:45:13 -0700

    fix path of local static files serve

--- server/server.js
+++ server/server.js
@@ -13,10 +13,10 @@ app.use("/record", records);
 // Serve static files from the React app
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production")
-    app.use(express.static(path.join(__dirname, "client/dist")));
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 
